33f339bf60140474d97b8736ce89630d93880637
Chaochao Wang <whale@dev.fish>
2024-08-06 14:30:47 +0800

    alert strategy

--- pkg/strategy/alert/strategy.go
+++ pkg/strategy/alert/strategy.go
@@ -2,7 +2,6 @@ package alert
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"github.com/c9s/bbgo/pkg/bbgo"
@@ -16,17 +15,13 @@ const ID = "alert"
 var log = logrus.WithField("strategy", ID)
 
 type Strategy struct {
-	Symbols  []string       `json:"symbol"`
-	Interval types.Interval `json:"interval"`
-	Prices   map[string][]types.KLine
-	Change   fixedpoint.Value `json:"change"`
+	Change   fixedpoint.Value `json:"change"` // 价格变化百分比
 	Notified map[string]time.Time
+	Prices   map[string]types.BookTicker
 }
 
 func init() {
 	// Register our struct type to BBGO
-	// Note that you don't need to field the fields.
-	// BBGO uses reflect to parse your type information.
 	bbgo.RegisterStrategy(ID, &Strategy{})
 }
 
@@ -35,62 +30,50 @@ func (s *Strategy) ID() string {
 }
 
 func (s *Strategy) Subscribe(ctx context.Context, session *bbgo.ExchangeSession) {
-	for _, symbol := range s.Symbols {
-		session.Subscribe(types.KLineChannel, symbol, types.SubscribeOptions{Interval: s.Interval})
-		s.fetchHistoricalData(ctx, symbol, session)
-	}
+	session.Subscribe(types.BookTickerChannel, "*", types.SubscribeOptions{})
 }
 
-func (s *Strategy) checkPriceChange(symbol string) {
-	prices := s.Prices[symbol]
-	if len(prices) < 20 {
-		return
-	}
+func (s *Strategy) checkPriceChange(symbol string, ticker types.BookTicker) {
+	/*
+		// 检查是否需要发送通知
+		if last, ok := s.Notified[symbol]; ok {
+			if last.Add(time.Minute * 30).After(time.Now()) {
+				return
+			}
+		}
 
-	if last, ok := s.Notified[symbol]; ok {
-		if last.Add(time.Minute * 30).After(time.Now()) {
+		initialTicker, exists := s.Prices[symbol]
+		if !exists {
+			// 如果没有初始价格记录，则记录当前价格
+			s.Prices[symbol] = ticker
 			return
 		}
-	}
+		init
 
-	initialPrice := prices[0].Close
-	currentPrice := prices[len(prices)-1].Close
-	priceChange := ((currentPrice.Float64() - initialPrice.Float64()) / initialPrice.Float64())
-	log.Infof("price change %.2f %.2f %.4f", initialPrice.Float64(), currentPrice.Float64(), priceChange)
+		initialPrice := initialTicker.Last
+		currentPrice := ticker.Last
+		priceChange := (currentPrice.Float64() - initialPrice.Float64()) / initialPrice.Float64()
+		log.Infof("price change for %s: %.2f -> %.2f = %.4f", symbol, initialPrice.Float64(), currentPrice.Float64(), priceChange)
 
-	if priceChange > s.Change.Float64() {
-		msg := fmt.Sprintf("Price of %s has increased by more than 30%% in the past 24 hours. Current price: %.2f", symbol, currentPrice.Float64())
-		bbgo.Notify(msg)
-		s.Notified[symbol] = time.Now()
-	}
-}
-func (s *Strategy) fetchHistoricalData(ctx context.Context, symbol string, session *bbgo.ExchangeSession) {
-	endTime := time.Now()
-	startTime := endTime.Add(-24 * time.Hour)
-
-	kLines, err := session.Exchange.QueryKLines(ctx, symbol, s.Interval, types.KLineQueryOptions{StartTime: &startTime, EndTime: &endTime})
-	if err != nil {
-		log.Printf("failed to fetch historical data for %s: %v", symbol, err)
-		return
-	}
-	s.Prices[symbol] = kLines
+		if priceChange > s.Change.Float64() {
+			msg := fmt.Sprintf("Price of %s has increased by more than %.2f%%. Current price: %.2f", symbol, s.Change.Float64()*100, currentPrice.Float64())
+			bbgo.Notify(msg)
+			s.Notified[symbol] = time.Now()
+			// 更新初始价格为当前价格
+			s.Prices[symbol] = ticker
+		}
+	*/
 }
 
 func (s *Strategy) Run(ctx context.Context, _ bbgo.OrderExecutor, session *bbgo.ExchangeSession) error {
-	s.Prices = make(map[string][]types.KLine)
+	s.Prices = make(map[string]types.BookTicker)
 	s.Notified = make(map[string]time.Time)
 
 	s.Subscribe(ctx, session)
 
-	session.MarketDataStream.OnKLineClosed(func(kline types.KLine) {
-		log.Infof("catchUp mode is enabled, updating grid orders...")
-		s.Prices[kline.Symbol] = append(s.Prices[kline.Symbol], kline)
-		if len(s.Prices[kline.Symbol]) > 20 {
-			s.Prices[kline.Symbol] = s.Prices[kline.Symbol][1:]
-		}
-		log.Printf("%s now: %.5f %.5f", kline.Symbol, kline.Close.Float64(), s.Prices[kline.Symbol][0].Open.Float64())
-		s.checkPriceChange(kline.Symbol)
+	session.MarketDataStream.OnBookTickerUpdate(func(ticker types.BookTicker) {
+		s.checkPriceChange(ticker.Symbol, ticker)
 	})
-	return nil
 
+	return nil
 }
